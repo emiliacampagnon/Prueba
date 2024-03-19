cdf2ae1b72f6cf69faddedc61e603e76e4f5f643
Emilia Campagnon <emiliacampagnon@gmail.com>
2024-03-19 10:07:50 -0300

    Hola

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer  from "./Footer.jsx";
+
+function App() {
+  let nombre = "Emita"
+
+  return <div> 
+    <h1>Hola {nombre} </h1>
+    <h2>como estas?</h2>
+    <Footer/>
+  </div>;
+  
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,6 @@
+const Footer = () =>{
+    
+    return <h1>Aca trabajariamos en Footer</h1>
+}
+
+export default Footer  
